d2165fa1dbf4a413364f8c832da0e89059ec326a
CanGeus <45945232+CanGeus@users.noreply.github.com>
2020-02-17 18:12:51 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,68 @@
+<?php
+// cek apakah tombol submit telah ditekan atau belum
+if( isset($_POST["submit"]) ) {
+    // cek username & password
+    if( $_POST["username"] == "admin" &&  $_POST["password"] == "admin")  {
+    // jika benar, redirect ke halaman selanjutnya
+        header("Location: index.html");
+        exit;
+    } else{
+// jika salah, tampilkan pesan kesalahan
+        $error = true;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+
+    <title>Login</title>
+</head>
+<body>
+
+
+
+
+    <div class="box">
+        <h1>Login</h1>
+
+        <form action="" method="post">
+
+            <div class="username">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="username" id="username" placeholder="Username">
+            </div>
+
+            <div class="password">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="password" id="password" placeholder="Password">
+            </div>
+
+            <?php if ( isset($error) ) :?>
+            <p>username atau password salah !</p>
+            <?php endif;?>
+            
+            <center>
+            <button tyepe="submit" name="submit">Login</button>
+            </center>
+
+        </form>
+
+    </div>
+
+    
+
+
+
+
+</body>
+</html>
